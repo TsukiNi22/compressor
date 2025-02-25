bc6f41d998d153f5ce1f2d4d8cee2d7386eb85fb
Mathias Dumoulin <mathias.dumoulin@epitech.eu>
2025-02-25 14:54:28 +0100

    fix: lib my_printf

--- lib/my/printf/specifiers/boolean.c
+++ lib/my/printf/specifiers/boolean.c
@@ -14,7 +14,7 @@ int specifiers_boolean(printf_data_t *data)
     if (!data)
         return err_prog(PTR_ERR, "In specifiers_boolean", KO);
     if (va_arg(data->ap, i_t)) {
-        if (my_printf("%O%Cfalse%R", data->fd, 0, 255, 0) == KO)
+        if (my_printf("%O%Ctrue%R", data->fd, 0, 255, 0) == KO)
             return err_prog(UNDEF_ERR, "In specifiers_i 1", KO);
     } else {
         if (my_printf("%O%Cfalse%R", data->fd, 255, 0, 0) == KO)
